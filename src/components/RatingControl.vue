1730b686dcb055c8bed943a01953a4f9bf40b892
Natsuki Hasegawa <oosacker@gmail.com>
2022-01-06 19:08:45 +1300

    custom forms

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li><button type="button" :class="{active: activeOption === 'poor'}" @click="activate('poor')">Poor</button></li>
+    <li><button type="button" :class="{active: activeOption === 'average'}" @click="activate('average')">Average</button></li>
+    <li><button type="button" :class="{active: activeOption === 'great'}" @click="activate('great')">Great</button></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      activeOption: null, 
+    }
+  },
+  methods: {
+    activate(option) {
+      this.activeOption = option
+      this.$emit('update:modelValue', option)
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+}
+button {
+  padding: 20px;
+  margin-left: 5px;
+}
+.active {
+  border-color: red;
+}
+button.active {
+  background-color: red;
+}
+</style>
